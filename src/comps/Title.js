840c693ac0d353fc2fdd467e4fdeb3aef90f0925
Borisigle <borisiglesias1@gmail.com>
2020-11-27 19:37:27 -0300

    first-commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1></h1>
+      <h2>Instafeed</h2>
+      <p>Personaliza tu feed</p>
+    </div>
+  )
+}
+
+export default Title;
