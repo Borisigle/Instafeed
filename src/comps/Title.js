53385758e420dd5f03dcb02c508464d659e40c23
Borisigle <borisiglesias1@gmail.com>
2020-11-27 23:52:48 -0300

    fi

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1></h1>
       <h2>Instafeed</h2>
       <p>Personaliza tu feed</p>
     </div>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,19 +2,21 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
-// Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAMNIorq48wynkSyFoLBC6SEI2xpmGZats",
-    authDomain: "instafeed-40e02.firebaseapp.com",
-    databaseURL: "https://instafeed-40e02.firebaseio.com",
-    projectId: "instafeed-40e02",
-    storageBucket: "instafeed-40e02.appspot.com",
-    messagingSenderId: "878713744116",
-    appId: "1:878713744116:web:b0b779e11ca17332338ca1"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAMNIorq48wynkSyFoLBC6SEI2xpmGZats",
+  authDomain: "instafeed-40e02.firebaseapp.com",
+  databaseURL: "https://instafeed-40e02.firebaseio.com",
+  projectId: "instafeed-40e02",
+  storageBucket: "instafeed-40e02.appspot.com",
+  messagingSenderId: "878713744116",
+  appId: "1:878713744116:web:b0b779e11ca17332338ca1"
+};
 
-  const projectStorage = firebase.storage();
-  const projectFirestore = firebase.firestore();
-  export { projectStorage, projectFirestore}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
